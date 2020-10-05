2d690ea854cf3a4d67b020c79749fe071d6a8afe
Tanner Austin <tanner.a.austin@gmail.com>
2020-10-05 14:41:58 -0500

    Formatting and structure

---
+++ script.js
@@ -0,0 +1,107 @@
+var queryUrl = "https://api.themoviedb.org/3/trending/all/day?api_key=9bfe03956c9070cdbcee94fe9384e956"
+
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+})
+
+// {
+// "genres": [
+//     {
+//         "id": 28,
+//         "name": "Action"
+            // If sunny
+//     },
+//     {
+//         "id": 12,
+//         "name": "Adventure"
+            // If cloudy
+//     },
+//     {
+//         "id": 16,
+//         "name": "Animation"
+            // If cloudy
+//     },
+//     {
+//         "id": 35,
+//         "name": "Comedy"
+            // If sunny
+//     },
+//     {
+//         "id": 80,
+//         "name": "Crime"
+            // If rainy 
+//     },
+//     {
+//         "id": 99,
+//         "name": "Documentary"
+            // If rainy
+//     },
+//     {
+//         "id": 18,
+//         "name": "Drama"
+            // if cloudy
+//     },
+//     {
+//         "id": 10751,
+//         "name": "Family"
+            // If cold
+//     },
+//     {
+//         "id": 14,
+//         "name": "Fantasy"
+            // if else
+//     },
+//     {
+//         "id": 36,
+//         "name": "History"
+            // if cloudy
+//     },
+//     {
+//         "id": 27,
+//         "name": "Horror"
+            // If past 9pm or if rainy
+//     },
+//     {
+//         "id": 10402,
+//         "name": "Music"
+            // If snowing
+//     },
+//     {
+//         "id": 9648,
+//         "name": "Mystery"
+            // If cloudy
+//     },
+//     {
+//         "id": 10749,
+//         "name": "Romance"
+            // if rainy
+//     },
+//     {
+//         "id": 878,
+//         "name": "Science Fiction"
+            // If else
+//     },
+//     {
+//         "id": 10770,
+//         "name": "TV Movie"
+//     },
+//     {
+//         "id": 53,
+//         "name": "Thriller"
+            // If past 9
+//     },
+//     {
+//         "id": 10752,
+//         "name": "War"
+            // If cold
+//     },
+//     {
+//         "id": 37,
+//         "name": "Western"
+            // If else
+//     }
+// ]
+// }
